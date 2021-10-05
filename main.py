f48bdc0fd27509c40b8e7988c8e6d65e3e45e35b
Anton Rösler <anton.roesler@stud.fra-uas.de>
2021-10-05 11:28:59 -0700

    Calculate all three directions

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import datetime
 
 distances = {
     'm': 1,
+    'meters': 1,
+    'meter': 1,
     'km': 1000,
+    'kilometer': 1000,
+    'kilometers': 1000,
     'miles': 1609.344
 }
 
@@ -18,26 +22,57 @@ def _get_time(**kwargs):
     h = kwargs.get('hours', 0)
     m = kwargs.get('minutes', 0)
     s = kwargs.get('seconds', 0)
-    return datetime.timedelta(hours=h, minutes=m, seconds=s)
+    t = datetime.timedelta(hours=h, minutes=m, seconds=s)
+    return t if t.total_seconds() > 0 else None
+
+
+def count_args(data):
+    """
+    Counts the number of elements in data that are not None
+    :param data: list type
+    :return: int
+    """
+    return sum(x is not None for x in data)
 
 
 class Run:
     def __init__(self, **kwargs):
-        self._meters: float = _get_meters(**kwargs)
+        self.meters: float = _get_meters(**kwargs)
         self.time: datetime.timedelta = _get_time(**kwargs)
+        self.pace: datetime.timedelta = kwargs.get('pace')
 
-    @property
-    def meters(self):
-        return self._meters
+        if count_args([self.meters, self.time, self.pace]) > 2:
+            raise TooManyArguments
+        if count_args([self.meters, self.time, self.pace]) < 2:
+            raise NotEnoughArguments
+
+        if self.meters is None:
+            self._calc_meters()
+        if self.time is None:
+            self._calc_time()
+        if self.pace is None:
+            self._calc_pace()
 
     @property
     def minutes(self):
         return self.time.total_seconds() / 60
 
-
-    @property
-    def pace(self):
+    def _calc_pace(self):
         sec = self.time.total_seconds()
-        p_s_m = sec/self._meters
+        p_s_m = sec / self.meters
         pace = p_s_m * 1000
-        return datetime.timedelta(seconds=pace)
+        self.pace = datetime.timedelta(seconds=pace)
+
+    def _calc_meters(self):
+        self.meters = (self.time.total_seconds() / self.pace.total_seconds()) * 1000
+
+    def _calc_time(self):
+        self.time = datetime.timedelta(seconds=(self.meters / 1000) * self.pace.total_seconds())
+
+
+class TooManyArguments(Exception):
+    """You must set exactly 2 of the 3 arguments [time, distance pace]"""
+
+
+class NotEnoughArguments(Exception):
+    """You must set exactly 2 of the 3 arguments [time, distance pace]"""
--- test/test_units.py
+++ test/test_units.py
@@ -2,7 +2,7 @@ import datetime
 
 import pytest
 
-from main import Run
+from main import Run, TooManyArguments, count_args, NotEnoughArguments
 
 
 def test_km_to_meter():
@@ -55,3 +55,52 @@ def test_get_pace_from_time_and_distance():
 
     run = Run(minutes=100, miles=12)
     assert round(run.pace.total_seconds()) == datetime.timedelta(minutes=5, seconds=11).total_seconds()
+
+
+def test_set_pace():
+    p = datetime.timedelta(minutes=4, seconds=45)
+    run = Run(pace=p)
+    assert run.pace == p
+
+
+def test_time_pace_to_distance():
+    run = Run(hours=1, pace=datetime.timedelta(minutes=5))
+    assert run.meters == 12000
+
+    run = Run(minutes=40, pace=datetime.timedelta(minutes=4, seconds=35))
+    assert round(run.meters) == 8727
+
+    run = Run(minutes=0, pace=datetime.timedelta(minutes=4, seconds=35))
+    assert round(run.meters) == 0
+
+
+def test_pace_distance_to_time():
+    run = Run(meters=7844, pace=datetime.timedelta(minutes=4, seconds=35))
+    assert round(run.time.total_seconds()) == 35 * 60 + 57
+
+    run = Run(meters=0, pace=datetime.timedelta(minutes=5, seconds=1))
+    assert round(run.time.total_seconds()) == 0
+
+
+def test_count_none():
+    assert count_args([None]) == 0
+    assert count_args([]) == 0
+    assert count_args([12, datetime.timedelta(seconds=2)]) == 2
+    assert count_args([None, None, None]) == 0
+    assert count_args([12, None, datetime.timedelta(seconds=2)]) == 2
+
+
+def test_too_many_args():
+    with pytest.raises(TooManyArguments):
+        Run(meters=20, pace=datetime.timedelta(minutes=5, seconds=3), minutes=30)
+
+
+def test_not_enough_args():
+    with pytest.raises(NotEnoughArguments):
+        Run(meters=20)
+    with pytest.raises(NotEnoughArguments):
+        Run(pace=datetime.timedelta(minutes=2))
+    with pytest.raises(NotEnoughArguments):
+        Run(hours=2, minutes=20)
+    with pytest.raises(NotEnoughArguments):
+        Run()
